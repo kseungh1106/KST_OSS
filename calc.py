0a500e0c54bd649be637041fbad35173f0a18f89
KST-KimSungTae <129771125+KST-KimSungTae@users.noreply.github.com>
2023-04-11 20:42:27 +0900

    Update calc.py
    문자 및 소수 입력시 코드 꼬임을 수정

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ def plus(x, y):
     return x+y
 def minus(x, y):
     return x-y
-def mul(x, y)
+def mul(x, y):
     return x*y
 def div(x, y):
     return x/y
@@ -13,32 +13,32 @@ def div(x, y):
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
+    while check>=1:        
         print("0: exit, 1: plus, 2: minus, 3: multiply, 4: divide")
         try:
-	    check = int(input())
+            check = int(input())
+            if check > 4:
+                print("Unsupported")
+            elif (check == 1 or 2 or 3 or 4):
+                print("First Number")
+                x = int(input())
+                print("Second Number")
+                y = int(input())
+                if check == 1:
+                    print("answer : ", plus(x,y))
+                elif check == 2:
+                    print("answer : ", minus(x,y))
+                elif check == 3:
+                    print("answer : ", mul(x,y))
+                elif check == 4:
+                    if y == 0:
+                        print("0으로 나눌 수 없습니다.")
+                    else :
+                        print("answer : ",div(x,y))
+            else:
+                print("Thank you")
         except ValueError:
-	    print("숫자를 입력하세요.")
-	if check > 4:
-	    print("Unsupported")
-	elif (check == 1 or 2 or 3 or 4):
-            print("First Number")
-            x = int(input())
-            print("Second Number")
-            y = int(input())
-            if check == 1:
-		print("answer : ", plus(x,y))
-	    elif check == 2:
-		print("answer : ", minus(x,y))
-	    elif check == 3:
-		print("answer : ", mul(x,y))
-	    elif check == 4:
-		if y == 0:
-			print("0으로 나눌 수 없습니다.")
-		else :
-			print("answer : ",div(x,y))
-        else:
-            print("Thank you")
+            print("정수인 숫자를 입력하세요.")
 
 if __name__ == "__main__":
     main()
